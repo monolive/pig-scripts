c22fa621b6ed5911f3e4a37bfd26a1d38f270c55
monolive <monoliv@gmail.com>
2013-08-29 16:15:33 -0700

    adding a function to convert to unix timestamp

--- fraud/udf/convert-date.py
+++ fraud/udf/convert-date.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 
 from datetime import datetime
+import calendar
+import time
 
 @outputSchema("date:DateTime")
 def date_to_iso_format(date):
   tuple_date = datetime.strptime(date,'%d-%b-%y')
   return tuple_date.strftime('%Y-%m-%d')
+
+@outputSchema("date:int")
+def date_to_int(date):
+#  return time.mktime(datetime.strptime(date,'%d-%b-%y').timetuple())
+  return calendar.timegm(time.strptime(date,'%d-%b-%y'))
